43969135bee50f4853abcd03efc00e7b52d474cc
Andrés Macías <58181194+amacias1711@users.noreply.github.com>
2025-06-03 09:12:25 -0500

    Remove unused endpoints and models

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 # app/main.py
 
 import uvicorn
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 from prometheus_fastapi_instrumentator import Instrumentator
 from slowapi import Limiter
 from slowapi.util import get_remote_address
@@ -9,17 +9,10 @@ from slowapi.middleware import SlowAPIMiddleware
 
 from app.utils.tracing import init_tracer, instrument_app
 from app.core.config import settings
-from app.api.auth import router as auth_router, JWTBearer
+from app.api.auth import router as auth_router
 
 # routers públicos
-from app.api.health     import router as health_router
-# routers protegidos
-from app.api.customers  import router as customers_router
-from app.api.products   import router as products_router
-from app.api.orders     import router as orders_router
-from app.api.invoices   import router as invoices_router
-from app.api.deliveries import router as deliveries_router
-from app.api.payments   import router as payments_router
+from app.api.health import router as health_router
 
 app = FastAPI(title="API Gateway SBO ↔ Odoo")
 
@@ -40,19 +33,6 @@ app.add_middleware(SlowAPIMiddleware)
 # 4.1 Públicos (no requieren token)
 app.include_router(health_router)      # /api/health
 app.include_router(auth_router)        # /api/token
-
-# 4.2 Protegidos (requieren JWT y rate-limit)
-secure_deps = [
-    Depends(JWTBearer()),              # valida el Bearer <token>
-    Depends(limiter.limit("1/second;60/minute")) # apila rate-limit también (opcional porque default_limits ya aplica)
-]
-
-app.include_router(customers_router, dependencies=secure_deps)
-app.include_router(products_router, dependencies=secure_deps)
-app.include_router(orders_router, dependencies=secure_deps)
-app.include_router(invoices_router, dependencies=secure_deps)
-app.include_router(deliveries_router, dependencies=secure_deps)
-app.include_router(payments_router, dependencies=secure_deps)
 
 if __name__ == "__main__":
     uvicorn.run(
